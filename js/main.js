9c69d8be869ac19fdcd1e901fb58cd754f3ef6e3
yuzai <a545905403@163.com>
2016-09-16 22:31:14 +0800

    change the span display to none at index.html,before is in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+	 $('#f_nav').hide();
   $("a").eq(0).click(function(){
 		$($("#m1")[0]).prependTo($('.cont'));
   });
@@ -14,4 +15,32 @@ jQuery(document).ready(function($){
 	$("a").eq(4).click(function(){
   $($("#m5")[0]).prependTo($('.cont'));
 });
+	$('#f_nav span').click(function(){
+		if($('#first_nav').css('display') =='none')
+		{
+			$('#first_nav ul').addClass('nav-stacked');
+			$('#first_nav').show();
+		}
+		else{
+			$('#first_nav ul').removeClass('nav-stacked');
+			$('#first_nav').hide();
+		}
+	});
+	// }
+	// $('#f_nav span').click(
+ //  	$(this).css('border','1px solid red');
+ //  	) 
+})
+$(window).resize(function(){
+	var x = $(window).width();
+	if(x<700)
+	{
+		$('#f_nav').show();
+		$('#first_nav').hide();
+	}
+	else
+	{
+		$('#f_nav').hide();
+		$('#first_nav').show();
+	}
 })
