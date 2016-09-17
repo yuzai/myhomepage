a55a025149386e8c0f36e82117078f62d7e66f44
yuzai <a545905403@163.com>
2016-09-17 12:13:00 +0800

    add a video to show and use fadein,fadeout to show or hide element

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
 jQuery(document).ready(function($){
 	 $('#f_nav').hide();
   $("a").eq(0).click(function(){
-		$($("#m1")[0]).prependTo($('.cont'));
+		var x = $(window).width();
+		$('.camera').fadeIn();
+	if(x<700)
+		$('#first_nav').hide();
+    $('.cont img,video').eq(0).fadeOut();
+    $($("#m1")[0]).prependTo($('.cont'));
+	$('#m1').fadeIn();
   });
   $("a").eq(1).click(function(){
-  	$($("#m2")[0]).prependTo($('.cont'));
+  	var x = $(window).width();
+  	$('.camera').fadeIn();
+  	if(x<700)
+		$('#first_nav').hide();
+	$('.cont img,video').eq(0).fadeOut();
+	$($("#m2")[0]).prependTo($('.cont'));
+	$('#m2').fadeIn();
   });
   $("a").eq(2).click(function(){
-  		$($("#m3")[0]).prependTo($('.cont'));
+  	var x = $(window).width();
+  	$('.camera').fadeIn();
+  	if(x<700)
+		$('#first_nav').hide();
+	$('.cont img,video').eq(0).fadeOut();
+	$($("#m3")[0]).prependTo($('.cont'));
+	$('#m3').fadeIn();
   });
 	$("a").eq(3).click(function(){
-  $($("#m4")[0]).prependTo($('.cont'));
+		$('.cont img,video').eq(0).fadeOut();
+	$($("#m6")[0]).prependTo($('.cont'));
+	$('#m6').fadeIn();
+	$('#m6').get(0).currentTime = 0;
+	$('.camera').fadeOut();
+  $('#m6').get(0).play();
+    	var x = $(window).width();
+  	if(x<700)
+		$('#first_nav').hide();
 });
 	$("a").eq(4).click(function(){
-  $($("#m5")[0]).prependTo($('.cont'));
+    	var x = $(window).width();
+  	if(x<700)
+		$('#first_nav').hide();
+	$('.camera').fadeIn();
+	$('.cont img,video').eq(0).fadeOut();
+	$($("#m4")[0]).prependTo($('.cont'));
+	$('#m4').fadeIn();
 });
 	$('#f_nav span').click(function(){
 		if($('#first_nav').css('display') =='none')
@@ -41,6 +73,7 @@ $(window).resize(function(){
 	else
 	{
 		$('#f_nav').hide();
+		$('#first_nav ul').removeClass('nav-stacked');
 		$('#first_nav').show();
 	}
 })
